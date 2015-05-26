120b4cbae9665f7d7d25fcfb3dbb85ff3e60f272
NatanGranillo <natan_granillo@yahoo.com>
2015-05-26 12:41:40 -0600

    UI Build 1.4 - update drawable

--- app/src/main/java/com/lok8/trackr/MainActivity.java
+++ app/src/main/java/com/lok8/trackr/MainActivity.java
@@ -4,64 +4,21 @@ import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.util.Log;
-import android.widget.RelativeLayout;
-import android.widget.Button;
-import android.graphics.Color;
-import android.widget.EditText;
+import android.view.Window;
+import android.view.View;
+import android.app.Activity;
 
 
 public class MainActivity extends ActionBarActivity {
 
-    private static final String TAG = "androidMessage";
+   // private static final String TAG = "androidMessage";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        //supportRequestWindowFeature(Window.FEATURE_INDETERMINATE_PROGRESS);
         super.onCreate(savedInstanceState);
 
 
-        //setContentView(R.layout.activity_main);
-        RelativeLayout trackrLayout = new RelativeLayout(this);
-
-
-        //Login Button
-        Button loginButton = new Button(this);
-        loginButton.setText("Login");
-        loginButton.setBackgroundColor(Color.YELLOW);
-
-        //Username input
-        EditText username = new EditText(this);
-        loginButton.setId(1);
-        username.setId(2);
-
-        RelativeLayout.LayoutParams buttonDetails = new RelativeLayout.LayoutParams(
-                RelativeLayout.LayoutParams.WRAP_CONTENT,
-                RelativeLayout.LayoutParams.WRAP_CONTENT
-        );
-
-        RelativeLayout.LayoutParams usernameDetails = new RelativeLayout.LayoutParams(
-                RelativeLayout.LayoutParams.WRAP_CONTENT,
-                RelativeLayout.LayoutParams.WRAP_CONTENT
-        );
-
-        usernameDetails.addRule(RelativeLayout.ABOVE, loginButton.getId());
-        usernameDetails.addRule(RelativeLayout.CENTER_HORIZONTAL);
-        usernameDetails.setMargins(0,0,0,50);
-
-        buttonDetails.addRule(RelativeLayout.CENTER_HORIZONTAL);
-        buttonDetails.addRule(RelativeLayout.CENTER_VERTICAL);
-
-        trackrLayout.addView(username, usernameDetails);
-
-
-
-
-        buttonDetails.addRule(RelativeLayout.CENTER_HORIZONTAL);
-        buttonDetails.addRule(RelativeLayout.CENTER_VERTICAL);
-
-        //Add widget to layout
-        trackrLayout.addView(loginButton);
-
-        setContentView(trackrLayout);
+        setContentView(R.layout.activity_main);
 
 
     }
